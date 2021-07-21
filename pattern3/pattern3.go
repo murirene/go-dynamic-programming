c1e3e5b6125baf26aee1a1646df7d54fe9996bd0
Rene <rene.murillo@ymail.com>
2021-07-21 08:09:57 -0700

    Added tabular staircase

--- pattern3/pattern3.go
+++ pattern3/pattern3.go
@@ -63,3 +63,25 @@ func staircaseRecursive(n int, dp [][]int) int {
 
 	return stepOne + stepTwo + stepThree
 }
+
+func StaircaseTabular(n int) int {
+	if n == 1 || n == 2 {
+		return n
+	}
+
+	if n <= 0 {
+		return 0
+	}
+
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	dp[3] = 4
+
+	for i := 4; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+	}
+
+	return dp[n]
+
+}
--- pattern_test/pattern3_test.go
+++ pattern_test/pattern3_test.go
@@ -20,12 +20,21 @@ func TestFib(t *testing.T) {
 }
 
 func TestStaircase(t *testing.T) {
-    if pattern3.StaircaseRecursive(4) != 7 {
-        t.Fatal("Fatal 3-2")
-    }
+	if pattern3.StaircaseRecursive(4) != 7 {
+		t.Fatal("Fatal 3-2")
+	}
 
+	if pattern3.StaircaseRecursive(5) != 13 {
+		t.Fatal("Fatal 3-3")
+	}
+}
 
-    if pattern3.StaircaseRecursive(5) != 13 {
-        t.Fatal("Fatal 3-3")
-    }
+func TestStaircaseTabular(t *testing.T) {
+	if pattern3.StaircaseTabular(4) != 7 {
+		t.Fatal("Fatal 3-2 tabular")
+	}
+
+	if pattern3.StaircaseTabular(5) != 13 {
+		t.Fatal("Fatal 3-3 tabular")
+	}
 }
